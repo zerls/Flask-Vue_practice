23eaab2419d55462d1ed4a67e1a3db94fb6d3f31
zerl <starss@outlook.com>
2018-12-09 21:03:18 +0800

    Echart

--- app/api/data.py
+++ app/api/data.py
@@ -1,7 +1,6 @@
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import urllib
 import time
-
 from flask import current_app, jsonify, request, url_for
 
 from . import api
@@ -35,10 +34,9 @@ def add_data(col):
     return jsonify({"code": code,
                     "msg": msg})
 
+
 # //数据删除
 # /api/version/data?token=_ [DELETE]
-
-
 @api.route('/data', methods=['DELETE'])
 # @admin_verify
 def del_data():
@@ -56,50 +54,125 @@ def del_data():
             db.session.delete(d)
             db.session.commit()
         elif time_start and place:
-            if time_slice is None: time_slice = 1
+            if time_slice is None:
+                time_slice = 1
             time = datetime.fromtimestamp(int(time_start))
             # d = db.session.query(Data).filter(Data.time.between(time, time+timedelta(seconds=int(time_slice))),
             # data_place=place, data_type=types)
-            ds = db.session.query(Data).filter(Data.time.between(time, time+timedelta(seconds=int(time_slice))))
-            for d in ds:        
+            ds = db.session.query(Data).filter(Data.time.between(
+                time, time+timedelta(seconds=int(time_slice))))
+            for d in ds:
                 db.session.delete(d)
             db.session.commit()
         else:
-            code, msg=403, "参数错误"
+            code, msg = 403, "参数错误"
     except Exception as e:
         print("ERROR: "+e.__str__())
         db.session.rollback()
-        code, msg=404, "无数据"
+        code, msg = 404, "无数据"
     return jsonify({"code": code,
                     "msg": msg})
 
 
+# TODO
+# //采集数据类型列表
+# /api/version/data [GET]  data/type/list
+@api.route('/data', methods=['POST'])
+# @user_verify
+def get_datatype(col):
+    try:
+        json = request.json
+        place = Place.query.filter_by(name=json['data']['place']).first()
+        type = Data_type.query.filter_by(name=json['data']['type']).first()
+        content = json['data']['content']
+        d = Data(content=content, data_type=type,
+                 data_place=place, data_collector=col)
+        db.session.add(d)
+        db.session.commit()
+        print(1)
+        code, msg = 200, 'OK'
+    # TODO png
+    except Exception as e:
+        print("ERROR: "+e.__str__())
+        db.session.rollback()
+        code, msg = 403, "添加失败"
+    return jsonify({"code": code,
+                    "msg": msg})
+
+
+# TODO
+# //历史数据获取
+# /api/version/data [GET]  data/type/list
+@api.route('/data/action/history', methods=['POST'])
+# @user_verify
+def get_dataHis():
+    try:
+        json = request.json
+        print(request.data)
+        place = json['place']
+        types = json['type']
+        time_start=int(json['sdate'])/1000
+        time_slice=int(json['edate'])/1000-time_start
+
+        place = urllib.parse.unquote(place)
+        types = urllib.parse.unquote(types)
+        p = Place.query.filter_by(name=place).first()
+        t = Data_type.query.filter_by(name=types).first()
+        if p and t:
+            time_ = datetime.fromtimestamp(int(time_start))
+
+            ds = db.session.query(Data).filter(Data.time.between(
+                time_, time_+timedelta(seconds=int(time_slice)))).filter_by(data_place=p, data_type=t).order_by(
+                Data.id).all()
+            data = []
+            for d in ds:
+                data.append(
+                    {"data": d.content, "time": time.mktime(d.time.timetuple())})
+            code, msg = 200, 'OK'
+            print(data)
+        else:
+            code, msg, data, times = 404, "无数据", [], ''
+    except Exception as e:
+        print("ERROR: "+e.__str__())
+        code, msg, data, times = 403, "参数错误", [], ''
+
+    return jsonify({"code": code,
+                    "msg": msg,
+                    "data": data,
+                    })
+
+# sdate: 1547395200000
+# edate: 1548691200000
 # //最近数据获取
 # /api/version/data/ [GET]
+
+
 @api.route('/data/action/last', methods=['GET'])
 # @user_verify
 def last_data():
     try:
-        place=request.args.get('place')
-        types=request.args.get('type')
-        place=urllib.parse.unquote(place)
-        types=urllib.parse.unquote(types)
-        p=Place.query.filter_by(name=place).first()
-        t=Data_type.query.filter_by(name=types).first()
+        place = request.args.get('place')
+        types = request.args.get('type')
+        sdate = request.args.get('sdate')
+        edate = request.args.get('edate')
+        place = urllib.parse.unquote(place)
+        types = urllib.parse.unquote(types)
+        p = Place.query.filter_by(name=place).first()
+        t = Data_type.query.filter_by(name=types).first()
         if p and t:
-            d=Data.query.filter_by(data_place=p, data_type=t).order_by(
+            d = Data.query.filter_by(data_place=p, data_type=t).order_by(
                 Data.id.desc()).first()
-            code, msg, data, times=200, 'OK', d.content, time.mktime(
+            code, msg, data, times = 200, 'OK', d.content, time.mktime(
                 d.time.timetuple())
             print(data)
         else:
-            code, msg, data, times=404, "无数据", [], ''
+            code, msg, data, times = 404, "无数据", [], ''
     except Exception as e:
         print("ERROR: "+e.__str__())
-        code, msg, data, times=403, "参数错误", [], ''
+        code, msg, data, times = 403, "参数错误", [], ''
 
     return jsonify({"code": code,
                     "msg": msg,
                     "data": data,
                     "time": times,
-    })
+                    })
